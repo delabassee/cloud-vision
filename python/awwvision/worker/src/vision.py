910c1044061542e9391e3c71204ef2b43db4fa18
David Delabassee <david.delabassee@gmail.com>
2016-12-14 11:28:21 +0100

    Update vision.py

--- python/awwvision/worker/src/vision.py
+++ python/awwvision/worker/src/vision.py
@@ -30,6 +30,45 @@ class VisionApi(object):
             'vision', 'v1', credentials=credentials,
             discoveryServiceUrl=DISCOVERY_URL)
 
+    
+    
+    ###########
+    def detect_label(self, image):
+        """Uses the Vision API to detect text in the given file.
+        """
+
+        batch_request = []
+
+        batch_request.append({
+            'image': {
+                'content': base64.b64encode(image).decode('UTF-8')
+             },
+               'features': [{
+                 'type': 'LABEL_DETECTION',
+                 'maxResults': 3,
+             }]
+        })
+
+        request = self.vision.images().annotate(
+            body={'requests': batch_request})
+
+        response = request.execute(num_retries=2)
+
+        label_responses = []
+
+        for r in response['responses']:
+            labels = [
+                x['description'] for x in r.get('labelAnnotations', [])]
+
+            label_responses.append(labels)
+
+        return label_responses
+    
+########  ////////
+    
+    
+    
+    
     def detect_labels(self, images, max_results=2, num_retries=3):
         """Uses the Vision API to detect text in the given file.
         """
